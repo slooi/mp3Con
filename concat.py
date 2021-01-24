0990e37c9c427a7deb594f9922fea4438fa31677
slooi <s.looi2@outlook.com>
2021-01-25 01:02:13 +1300

    create combine folder if it does not exist

--- concat.py
+++ concat.py
@@ -1,6 +1,9 @@
 import os
 folder_name = "combine"
 
+if not os.path.exists(folder_name):
+	os.mkdir(folder_name)
+
 namesFile = open("filenames.txt","w")
 names = ["file './{}/{}'\n".format(folder_name,filename) for filename in os.listdir("./{}/".format(folder_name))]
 print(names)
